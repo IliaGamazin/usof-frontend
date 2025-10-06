2f1ef575ee9a29f301bd1d4e39bb7cbeff0789ae
IliaGamazin <iliagamazin@gmail.com>
2025-10-06 11:36:41 +0000

    mobile-friendly header

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,3 +1,5 @@
+import { useState, useRef, useEffect } from 'react';
+
 import Button from "../../common/Button.jsx";
 import styles from "./Header.module.css";
 
@@ -7,40 +9,82 @@ import pencilIcon from '../../../assets/Pencil_icon.svg';
 import mangoIcon from '../../../assets/mango.svg';
 
 export default function Header() {
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+    const searchRef = useRef(null);
+    const inputRef = useRef(null);
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (searchRef.current && !searchRef.current.contains(event.target)) {
+                setIsSearchOpen(false);
+            }
+        }
+
+        if (isSearchOpen) {
+            document.addEventListener('mousedown', handleClickOutside);
+            if (inputRef.current) inputRef.current.focus();
+            document.body.style.overflow = "hidden";
+        }
+        else {
+            document.body.style.overflow = "";
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+            document.body.style.overflow = "";
+        };
+    }, [isSearchOpen]);
+
     return (
-        <header className={styles.header}>
-            <ul className={styles.headerList}>
-                <li className={styles.titleBlock}>
-                    <h2 className={styles.title}>MangOverflow</h2>
-                    <img className={styles.icon} src={mangoIcon} alt=""/>
-                </li>
-                <li>
-                    <search className={styles.searchContainer}>
-                        <input
-                            type="text"
-                            placeholder="Search..."
-                            className={styles.searchInput}
-                        />
-                    </search>
-                </li>
-                <li className={styles.buttonBlock}>
-                    <nav className={styles.buttonBar}>
-                        <Button className={styles.svgButton}>
-                            <img className={styles.icon} src={searchIcon} alt="search_icon.svg"/>
-                        </Button>
-                        <Button className={styles.svgButton}>
-                            <img className={styles.icon} src={bellIcon} alt="search_icon.svg"/>
-                        </Button>
-                        <Button className={styles.writeButton}>
-                            <img className={styles.icon} src={pencilIcon} alt="search_icon.svg"/>
-                            <span>Write a post</span>
-                        </Button>
-                        <Button className={styles.authButton}>
-                            Log in
-                        </Button>
-                    </nav>
-                </li>
-            </ul>
-        </header>
+        <>
+            {isSearchOpen && (
+                <div
+                    className={styles.filter}
+                    onClick={() => setIsSearchOpen(false)}
+                />
+            )}
+            <header className={styles.header}>
+                <ul className={styles.headerList}>
+                    <li className={styles.titleBlock}>
+                        <h2 className={styles.title}>Mangoflow</h2>
+                        <img className={styles.icon} src={mangoIcon} alt=""/>
+                    </li>
+                        <li
+                            ref={searchRef}
+                            className={styles.searchWrapper}
+                        >
+                            {isSearchOpen && (
+                                <search className={styles.searchContainer}>
+                                    <input
+                                        ref={inputRef}
+                                        type="text"
+                                        placeholder="Search..."
+                                        className={styles.searchInput}
+                                    />
+                                </search>
+                            )}
+                        </li>
+                    <li className={styles.buttonBlock}>
+                        <nav className={styles.buttonBar}>
+                            <Button
+                                className={styles.svgButton}
+                                onClick={() => setIsSearchOpen(!isSearchOpen)}
+                            >
+                                <img className={styles.icon} src={searchIcon} alt="search_icon.svg"/>
+                            </Button>
+                            <Button className={styles.svgButton}>
+                                <img className={styles.icon} src={bellIcon} alt="bell_icon.svg"/>
+                            </Button>
+                            <Button className={styles.writeButton}>
+                                <img className={styles.icon} src={pencilIcon} alt="pencil_icon.svg"/>
+                                <span>Write a post</span>
+                            </Button>
+                            <Button className={styles.authButton}>
+                                Log in
+                            </Button>
+                        </nav>
+                    </li>
+                </ul>
+            </header>
+        </>
     );
 }
